b0c5a5b163784e831fe02238f8705523df55c02f
cablej <jckcable@me.com>
2015-06-06 15:41:21 -0500

    Added views

--- arkaliv/views.py
+++ arkaliv/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import File
 
 # Create your views here.
 
 def files_list(request):
-	return render(request, 'files/files_list.html')
+	files = File.objects.filter(published_date__lt=timezone.now()).order_by('published_date')
+	return render(request, 'files/files_list.html', {'files': files})
